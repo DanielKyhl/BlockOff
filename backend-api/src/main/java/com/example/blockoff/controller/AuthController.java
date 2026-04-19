4ba867a7cf6b398f4ee99ed0a43b3f47d9563015
Mythical2305 <146701643+Mythical2305@users.noreply.github.com>
2026-04-19 10:50:23 +0200

    Update CORS configuration in AuthController

--- backend-api/src/main/java/com/example/blockoff/controller/AuthController.java
+++ backend-api/src/main/java/com/example/blockoff/controller/AuthController.java
@@ -8,10 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {
-        "http://localhost:5173",
-        "chrome-extension://lkpnjncioceiadifnbmeaioognfcgboc"
-})
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:8080",
+                        "chrome-extension://lkpnjncioceiadifnbmeaioognfcgboc"})
 public class AuthController {
 
     @Autowired
